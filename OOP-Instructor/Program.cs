598c3dc9cae1112249c4f44ef6188ad4c0d7d2a0
Honk <honk@honk.com>
2024-02-22 14:58:22 -0800

    Classes discussion, twenty one final breakdown.

--- OOP-Instructor/Program.cs
+++ OOP-Instructor/Program.cs
@@ -2,107 +2,15 @@
 
 class Program
 {
-    class MyClass
-    {
-        public int SomeValue;
-    }
-
-    // "structure"
-    struct MyStruct
-    {
-        public int SomeValue;
-    }
-
-    class Material
-    {
-        public Color theColor;
-    }
-
-    struct Color
-    {
-        int r, g, b, a;
-    }
-
-    class User
-    {
-        string username, displayName, profileDescription;
-        int cash;
-    }
-
-    struct Coordinate
-    {
-        int latitude, longitude;
-    }
-
-    class Pokemon
-    {
-        string name, type;
-        List<string> abilities;
-    }
-
-    struct PokemonEntity
-    {
-        public Vector2 Position, Rotation;
-
-        public Pokemon pokemon;
-    }
-
     private static void Main()
     {
-        // an "instance" is an entity that has been created
-        // in your program's "world",
-        // whereas a class or struct is a template or concept of
-        // how an object is defined.
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine(i);
+        }
 
-        // Classes are "pass by REFERENCE", in that a new copy is
-        // not created when they are assigned to new variables.
-
-        // Structs are "pass by VALUE"; they are FULLY copied when
-        // they are assigned to a new variable.
-
-        // These rules also apply when passing them into functions.
-
-        //MyStruct aClass = new MyStruct();
-
-        //aClass.SomeValue = 5;
-
-        //MyStruct bClass = aClass;
-
-        //bClass.SomeValue = 3;
-
-        //Console.WriteLine($"a:{aClass.SomeValue} b:{bClass.SomeValue}");
-
-        //MyStruct b = new MyStruct();
-
-        //MyClass a = new MyClass();
-        //a.SomeValue = 5;
-
-        //Modify(a);
-
-        //Console.WriteLine(a.SomeValue);
-
-        // a:3 b:3
-        // a:5 b:3
+        Console.WriteLine("All done!");
     }
-
-    private static void Modify(MyClass myClass)
-    {
-        myClass.SomeValue = 100000;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     struct Position
     {
---
+++ OOP-Instructor/TwentyOneDiscussion.cs
@@ -0,0 +1,67 @@
+﻿class TwentyOneDiscussion
+{
+
+    // public static readonly string[] Suits = new string[] { "Hearts", "Spades", "Clubs", "Diamonds" };
+
+    // The identity of a card is:
+    // its Suit + Name
+    // e.g., "Five of Clubs"
+    // Each card also has a score
+    enum Suits { Hearts, Spades, Clubs, Diamonds }
+
+    class Card
+    {
+        public string Name;     // Ace, 2, 6, Jack, etc.
+        public Suits Suit;    // 0, 1, 2, 3
+        public int Score;       // 1, 2, 6, 10 etc.
+        // ...
+    }
+
+    // int score;
+
+    class Player
+    {
+        // Hand or,
+        // Collection<Cards>, Name
+    }
+
+    class GameState
+    {
+        // Players, Dealer, Who'sTurn
+    }
+
+    class Deck
+    {
+        // 52 Cards
+    }
+
+    class Hand // as in, "Hand of cards"
+    {
+        // Collection<Card>
+
+        public int Score()
+        {
+            // for each Card, sum up the score, return the value
+            return 0;
+        }
+    }
+
+    public static void Execute()
+    {
+        List<Card> deck =
+        [
+            new Card() { Name = "Ace", Score = 1, Suit = Suits.Hearts },
+            new Card() { Name = "Ace", Score = 1, Suit = Suits.Spades },
+            new Card() { Name = "Ace", Score = 1, Suit = Suits.Diamonds },
+        ];
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 13; j++)
+            {
+
+            }
+        }
+    }
+}
+
