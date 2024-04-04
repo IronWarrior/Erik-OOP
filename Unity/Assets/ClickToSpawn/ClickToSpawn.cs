8735f47a4acf5ed2c32eee249c902b0a5465c8d5
Honk <honk@honk.com>
2024-04-04 15:01:51 -0700

    Click to spawn challenge solution.

--- Unity/Assets/ClickToSpawn/ClickToSpawn.cs
+++ Unity/Assets/ClickToSpawn/ClickToSpawn.cs
@@ -3,33 +3,37 @@ using UnityEngine.InputSystem;
 
 public class ClickToSpawn : MonoBehaviour
 {
+    public GameObject prefab;
     public Camera cam;
 
     private void Update()
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            Debug.Log("Clicked!");
-        }
+            Vector2 mousePosition = Mouse.current.position.value;
 
-        Vector2 mousePosition = Mouse.current.position.value;
+            // Get the mouse position of the camera's "box",
+            // and then get a "ray" that points out from the camera
+            Ray ray = cam.ScreenPointToRay(mousePosition);
 
-        // Get the mouse position of the camera's "box",
-        // and then get a "ray" that points out from the camera
-        Ray ray = cam.ScreenPointToRay(mousePosition);
+            //                                "out" means that this variable is empty when passed in,
+            //                                 but will be filled by the function itself
+            bool didHit = Physics.Raycast(ray, out RaycastHit hitInfo);
 
-        //                                "out" means that this variable is empty when passed in,
-        //                                 but will be filled by the function itself
-        bool didHit = Physics.Raycast(ray, out RaycastHit hitInfo);
+            if (didHit == true)
+            {
+                Debug.Log($"Object: {hitInfo.collider.name} position: {hitInfo.point}");
 
-        if (didHit == true)
-        {
-            Debug.Log($"Object: {hitInfo.collider.name} position: {hitInfo.point}");
-        }
+                if (hitInfo.collider.name == "Plane")
+                {
+                    Instantiate(prefab, hitInfo.point, Quaternion.identity);
+                }
+            }
 
-        Color rayColor = didHit ? Color.red : Color.green;
+            Color rayColor = didHit ? Color.red : Color.green;
 
-        // a "ray" is just a position and a direction.
-        Debug.DrawLine(ray.origin, ray.direction * 20f, rayColor);
+            // a "ray" is just a position and a direction.
+            Debug.DrawLine(ray.origin, ray.direction * 20f, rayColor, 1f);
+        }        
     }
 }
